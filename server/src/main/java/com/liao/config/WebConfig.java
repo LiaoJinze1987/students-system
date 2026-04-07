a07e5abd0c65c99d763f86b8275bbe049463133c
Liao <liaoassin1987@gmail.com>
2026-04-07 18:29:38 +0900

    fix: allow cross request

--- server/src/main/java/com/liao/config/WebConfig.java
+++ server/src/main/java/com/liao/config/WebConfig.java
@@ -2,7 +2,9 @@ package com.liao.config;
 
 import com.liao.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -24,5 +26,19 @@ public class WebConfig implements WebMvcConfigurer {
         );
         //不拦截登录和注册接口
         registry.addInterceptor(loginInterceptor).excludePathPatterns(excludePaths);
+    }
+
+    /**后端跨域配置，允许跨域访问*/
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")  // 允许所有来源
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
